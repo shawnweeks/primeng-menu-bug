93bc13c3e95210c4dc6d214d6449e70d7d6d1ca5
Shawn Weeks <sweeks@Shawns-MacBook-Pro.local>
2024-12-24 10:34:06 -0600

    Adding test menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { Menu } from 'primeng/menu';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Menu],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'primeng-menu-bug';
+  items: MenuItem[] | undefined;
+
+    ngOnInit() {
+        this.items = [
+            { label: 'New', icon: 'pi pi-plus' },
+            { label: 'Search', icon: 'pi pi-search' },
+            { label: 'Search', icon: 'pi pi-search' },
+            { label: 'Search-Hidden', icon: 'pi pi-search', visible: false },
+            { label: 'Search', icon: 'pi pi-search' },
+            { label: 'Search', icon: 'pi pi-search' },
+            { label: 'Search', icon: 'pi pi-search' },
+        ];
+    }
 }
